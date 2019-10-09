226efc57a259b65285bcededb25fc1884962c566
ZeroJ <pugssur@gmail.com>
2019-10-09 11:36:59 +0800

    三数之和问题

---
+++ Algorithm/ThreeSum.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+'''
+    给定一个包含 n 个整数的数组 nums ，判断 nums 中是否存在三个元素 a，b，c ，
+    使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+
+    注意： 答案中不可以包含重复的三元组。
+
+    例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+    满足要求的三元组集合为：
+    [
+    [-1, 0, 1],
+    [-1, -1, 2]
+    ]
+
+'''
+
+class Solution(object):
+    def threeSum(self, nums):
+        “””
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        “””
+        res = []
+        nums.sort()
+        for i in xrange(0, len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = 0 - nums[i]
+            start, end = i + 1, len(nums) - 1
+            while start < end:
+                if nums[start] + nums[end] > target:
+                    end -= 1
+                elif nums[start] + nums[end] < target:
+                    start += 1
+                else:
+                    res.append((nums[i], nums[start], nums[end]))
+                    end -= 1
+                    start += 1
+                    while start < end and nums[end] == nums[end + 1]:
+                        end -= 1
+                    while start < end and nums[start] == nums[start - 1]:
+                        start += 1
+        return res
